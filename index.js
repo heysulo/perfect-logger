35add687b1b0c2ea30b22dc8b822acf2dd57acc2
Sulochana Kodituwakku <sulochana.456@live.com>
2018-08-04 22:25:09 +0530

    Add file writing support
    Signed-off-by: Sulochana Kodituwakku <sulochana.456@live.com>

--- index.js
+++ index.js
@@ -5,15 +5,32 @@ function time() {
     return "time";
 }
 
-logger.log("Starting Server", "info");
 logger.addStatusCode("try", "TRY", false);
+logger.setTimeZone("Asia/Colombo");
+logger.setLogDirectory("./NodeLogs");
+logger.setLogFileName("UCSCRES");
+logger.setMaximumLogSize(10000);
+logger.setApplicationInfo({
+    name: "UCSC Results Center",
+    banner: "Copyright 2080 Team whileLOOP Incorporated",
+    version: "1.0.0"
+});
+logger.initialize();
+logger.log("Starting Server", "info");
 logger.info("info");
 logger.warn("warn");
+logger.switchLogs();
 logger.crit("crit");
 logger.try("Helloooooooo");
 
+let i = 0;
+
+
 //create a server object:
 http.createServer(function (req, res) {
+    for(i = 0; i < 100; i++){
+        logger.info(`Logline ${i}`);
+    }
     res.write('Hello World!'); //write a response to the client
     res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
--- modules/node-logger.js
+++ modules/node-logger.js
@@ -1,14 +1,31 @@
+const fileSystem = require('fs');
+const loggerData = {
+    version: "1.0.0.0"
+};
+
 // Standard Settings ******************************************************************************
 let standardStatusCodeLength = -1;
 let disableStatusCodePadding = false;
 let userTimeZone = "UTC";
 let getTime = undefined;
 let getDate = undefined;
+let logFileName = "ApplicationLog";
+let logFileNameSuffix;
+let logsDirectory = "./logs";
+let logNumber = 0;
+let maxLogSize = 10000;
+let currentLogFile;
+let previousLogFile = false;
 let statusCodeAliases = {
     info : { code: "INFO", writeToDatabase: false },
     warn : { code: "WARN", writeToDatabase: true },
     crit : { code: "CRIT", writeToDatabase: true },
     debug : { code: "DEBG", writeToDatabase: false },
+};
+let applicationInfo = {
+    name: "NodeJS Logging System",
+    banner: "Copyright 2018 Team whileLOOP",
+    version: "1.0"
 };
 
 // Internal Functions *****************************************************************************
@@ -51,6 +68,72 @@ function getStatusCodeToString(statusCode) {
 function writeLogLine(message, alias, databaseObj = { writeToDatabase : false }) {
     let logMessage = `${getDate()} | ${getTime()} | ${getStatusCodeToString(alias)} | ${message}`;
     console.log(logMessage);
+
+    if (getLogSize() > maxLogSize)
+        logSwitch();
+
+    fileSystem.appendFile(currentLogFile,
+        logMessage + '\n',
+        function(err) {
+            if(err) {
+                console.log(err);
+        }
+    });
+
+}
+//*************************************************************************************************
+function getLogSize() {
+    const stats = fileSystem.statSync(currentLogFile);
+    return stats.size;
+}
+
+//*************************************************************************************************
+function writeToLogFile(message) {
+    fileSystem.appendFile(currentLogFile,
+        message + '\n',
+        function(err) {
+            if(err) {
+                console.log(err);
+            }
+        }
+     );
+}
+
+//*************************************************************************************************
+function logSwitch() {
+    logNumber += 1;
+    if (!fileSystem.existsSync(logsDirectory)){
+        fileSystem.mkdirSync(logsDirectory);
+    }
+
+    let newLogFileNameSuffix = (new Date()).getTime();
+
+    if (previousLogFile)
+        writeToLogFile(`Continuing to ${logFileName}.${newLogFileNameSuffix}.log`);
+
+    previousLogFile = logFileNameSuffix ? `${logFileName}.${logFileNameSuffix}.log` : undefined;
+    logFileNameSuffix = newLogFileNameSuffix;
+    currentLogFile = `${logsDirectory}/${logFileName}.${logFileNameSuffix}.log`;
+
+    let logFileHeader = ``+
+    `******************************************************************************************\n` +
+    `**\n` +
+    `** ${applicationInfo.name} Logging System\n` +
+    `** Application Version          : ${applicationInfo.version}\n` +
+    `** Node-Logger Version          : ${loggerData.version}\n` +
+    `** Application Start Time (UTC) : ${applicationInfo.startTime}\n` +
+    `**\n` +
+    `** ${applicationInfo.banner}\n` +
+    `**\n` +
+    `******************************************************************************************\n` +
+    `**\n` +
+    `** Log Number               : #${logNumber}\n` +
+    `** Continuing from Log File : ${previousLogFile ? previousLogFile : 'None'}\n` +
+    `** Log Start Time (UTC)     : ${getTime()} ${getDate()}\n` +
+    `**\n` +
+    `******************************************************************************************`;
+
+    writeToLogFile(logFileHeader);
 }
 
 //*************************************************************************************************
@@ -116,6 +199,12 @@ exports.setDateFunction = function (userFunction) {
 };
 
 //*************************************************************************************************
+/**
+ * Log a message
+ * @param message
+ * @param alias
+ * @param databaseObj
+ */
 exports.log = function (message, alias, databaseObj = { writeToDatabase : false }) {
     writeLogLine(message, alias, databaseObj);
 };
@@ -129,13 +218,71 @@ exports.setTimeZone = function (timeZone) {
     userTimeZone = timeZone;
 };
 
-// Module Initializations *************************************************************************
-getTime = getTimeFunction;
-getDate = getDateFunction;
+//*************************************************************************************************
+/**
+ * Manually switch log files
+ */
+exports.switchLogs = function () {
+    logSwitch();
+};
 
-// Dynamic function generation for each code
-Object.keys(statusCodeAliases).forEach(function (key) {
-    exports[key] = function (message, databaseObj = { writeToDatabase : false }) {
-        writeLogLine(message, key, databaseObj);
-    }
-});
+//*************************************************************************************************
+/**
+ * Set the file name of the log files
+ * @param fileName - File name
+ */
+exports.setLogFileName = function (fileName) {
+    logFileName = fileName;
+};
+
+//*************************************************************************************************
+/**
+ * Set the location for the log files
+ * @param directory - Path
+ */
+exports.setLogDirectory = function (directory) {
+    logsDirectory = directory;
+};
+
+//*************************************************************************************************
+/**
+ * Set the maximum log file size in bytes which will be used for automatic log switching
+ * @param size - Size in Bytes
+ */
+exports.setMaximumLogSize = function (size) {
+    maxLogSize = size;
+};
+
+//*************************************************************************************************
+/**
+ * Set application information which will be used to create the log file banner
+ * Example :
+ * {
+ *      name: "Facebook Incorporated",
+ *      banner: "Copyright Facebook © 2018",
+ *      version: "12.65.2"
+ * }
+ * @param infoObject
+ */
+exports.setApplicationInfo = function (infoObject) {
+    applicationInfo = Object.assign({}, infoObject);
+};
+
+//*************************************************************************************************
+exports.initialize = function () {
+    if (!getTime)
+        getTime = getTimeFunction;
+
+    if (!getDate)
+        getDate = getDateFunction;
+
+    // Dynamic function generation for each code
+    Object.keys(statusCodeAliases).forEach(function (key) {
+        exports[key] = function (message, databaseObj = { writeToDatabase : false }) {
+            writeLogLine(message, key, databaseObj);
+        }
+    });
+
+    applicationInfo.startTime = `${getTime()} ${getDate()}`;
+    logSwitch();
+};
